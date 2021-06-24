154fea7573951238d1ca475c3133b25f4e536a13
Basel <mrbasel555@gmail.com>
2021-06-24 17:27:53 +0300

    add validation & error msgs

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const pgp = require("pg-promise")();
 const { nanoid } = require("nanoid");
+const cookieParser = require("cookie-parser");
+
+const { isValidUrl } = require("../helpers.js");
 
 const db = pgp(process.env.DATABASE_URL);
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.render("index.html", { title: "Express" });
+  const errors = req.cookies["err"];
+  res.clearCookie("err", { httpOnly: true });
+  
+  res.render("index.html", { title: "Express", errors: errors });
 });
 
 router.get("/url/:token", function (req, res, next) {
@@ -27,15 +33,23 @@ router.get("/:urlId", async function (req, res, next) {
 
 router.post("/", function (req, res, next) {
   const url = req.body.url;
-  if (!url) res.status(500).end();
 
-  const urlToken = nanoid(8);
-  db.any("INSERT INTO links(url_id, destination_url) VALUES ($1, $2)", [
-    urlToken,
-    url,
-  ]);
+  if (!isValidUrl(url)) {
+    res.cookie("err", "Invalid URL")
+    res.redirect("/");
+    return;
+  }
 
-  res.redirect("/url/" + urlToken);
+  if (url === "" || url == undefined) res.status(400).send("URL missing");
+  else {
+    const urlToken = nanoid(8);
+    db.any("INSERT INTO links(url_id, destination_url) VALUES ($1, $2)", [
+      urlToken,
+      url,
+    ]);
+
+    res.redirect("/url/" + urlToken);
+  }
 });
 
 module.exports = router;
