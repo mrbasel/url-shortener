5b4a45a22937d89554ebf3d26230879cb7774691
Basel <mrbasel555@gmail.com>
2021-07-03 04:13:31 +0300

    api now requires api key

--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,12 @@ const express = require("express");
 const { nanoid } = require("nanoid");
 
 const { isValidUrl } = require("../helpers.js");
-const { Link } = require("../models.js");
+const { Link, User } = require("../models.js");
 
 const router = express.Router();
 
 // Create link route
-router.post("/", async function (req, res, next) {
+router.post("/", isAuthorized, async function (req, res, next) {
   const url = req.body.url;
 
   if (!isValidUrl(url)) {
@@ -42,5 +42,35 @@ router.post("/", async function (req, res, next) {
     });
   }
 });
+
+async function isAuthorized(req, res, next) {
+  const apiKey = req.get("x-api-key");
+
+  if (!apiKey)
+    return res.status(401).json({
+      status: "fail",
+      message: "No api key provided",
+    });
+
+  try {
+    const user = await User.findOne({
+      where: {
+        apiKey: apiKey,
+      },
+    });
+
+    if (!user) {
+      res.status(400).end();
+    }
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({
+      status: "error",
+      message: "Something went wrong, try again later",
+    });
+  }
+
+  next();
+}
 
 module.exports = router;
