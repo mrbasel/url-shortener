2b1d9d9370c68529601c005c0d25341807e2ba59
mrbasel <mrbasel555@gmail.com>
2021-09-08 22:13:02 +0300

    implement all link controller methods

--- server/src/routes/link.js
+++ server/src/routes/link.js
@@ -5,7 +5,10 @@ const { isLoggedIn } = require("../middlewares");
 const router = express.Router();
 
 router.get("/:urlId", LinkController.visitLink);
-router.post("/trim", LinkController.shortenLink);
+
 router.get("/links", isLoggedIn, LinkController.getLinks);
+router.post("/trim", LinkController.shortenLink);
+router.put("/links/:id", isLoggedIn, LinkController.updateLink);
+router.delete("/links/:id", isLoggedIn, LinkController.deleteLink);
 
 module.exports = router;
